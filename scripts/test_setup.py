6990acc072e7226c2422429e90405d3fe0103656
Harsh Kumar <harshbkp123@gmail.com>
2025-01-06 21:50:36 +0530

    Initial setup for human detector project

---
+++ scripts/test_setup.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import cv2
+import tensorflow as tf
+import matplotlib
+
+print("Setup is ready!")
+print("Numpy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("OpenCV:", cv2.__version__)
+print("TensorFlow:", tf.__version__)
+print("Matplotlib:", matplotlib.__version__)
